fea10f3ef1ab69f847a31afe0581fddb345bf998
SWKHandsome <b240150B@sc.edu.my>
2025-12-26 19:32:37 +0800

    Update app.php

--- config/app.php
+++ config/app.php
@@ -3,9 +3,39 @@
 declare(strict_types=1);
 
 const APP_NAME = 'Campus Parcel & Lost & Found';
-const BASE_URL = '/Web%20Development/2025C-Web-Development-Group-Project';
 const BASE_PATH = __DIR__ . '/..';
 const UPLOAD_DIR = BASE_PATH . '/uploads';
 const SESSION_COOKIE_NAME = 'campus_portal_session';
+
+$baseUrl = getenv('APP_BASE_URL');
+
+if ($baseUrl === false || $baseUrl === '') {
+	$projectPath = str_replace('\\', '/', realpath(BASE_PATH) ?: '');
+	$documentRoot = isset($_SERVER['DOCUMENT_ROOT'])
+		? str_replace('\\', '/', realpath((string) $_SERVER['DOCUMENT_ROOT']) ?: '')
+		: '';
+
+	if ($projectPath !== '' && $documentRoot !== '' && strpos($projectPath, $documentRoot) === 0) {
+		$relativePath = trim(substr($projectPath, strlen($documentRoot)), '/');
+
+		if ($relativePath !== '') {
+			$segments = array_filter(explode('/', $relativePath), 'strlen');
+			$encodedSegments = array_map('rawurlencode', $segments);
+			$baseUrl = '/' . implode('/', $encodedSegments);
+		} else {
+			$baseUrl = '';
+		}
+	} else {
+		$baseUrl = '';
+	}
+} else {
+	$baseUrl = rtrim($baseUrl, '/');
+
+	if ($baseUrl !== '' && $baseUrl[0] !== '/') {
+		$baseUrl = '/' . $baseUrl;
+	}
+}
+
+define('BASE_URL', $baseUrl);
 
 date_default_timezone_set('Asia/Kuala_Lumpur');
